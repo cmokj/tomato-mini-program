9abe1e3ecfa8c1c2208577da7fca3aecaaef192b
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-09-27 16:50:51 +0800

    首页使用 button 组件，生成一个『创建任务』按钮

--- components/button/button.js
+++ components/button/button.js
@@ -1,3 +1,16 @@
 Component({
-  
+  properties: {
+    size: {
+      type: String,
+      value: ''
+    },
+    type: {
+      type: String,
+      value: 'default'
+    },
+    text: {
+      type: String,
+      value: ''
+    }
+  }
 })
