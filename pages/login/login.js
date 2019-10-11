35ec8c32808019711371b59a894393529235cb62
cmokj <root@chenminjiadeMacBook-Pro.local>
2019-10-11 21:40:31 +0800

    完成一键登录功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,29 @@
-// pages/login/login.js
+const { http } = require("../../lib/http.js");
+const { app_id, app_secret } = getApp().globalData;
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  
+  // code, iv, encrypted_data, app_id, app_secret
+  login: function(e) {
+    let code;
+    let iv = e.detail.iv;
+    let encrypted_data = e.detail.encryptedData;
+    wx.login({
+      success(res) {
+        if (res.code) {
+          code = res.code;
+          http.post('/sign_in/mini_program_user', { code, iv, encrypted_data, app_id, app_secret }).then((response) => {
+            let event = response.response;
+            wx.setStorageSync('user', event.data.resource);
+            wx.setStorageSync('X-token', event.header["X-token"]);
+            wx.reLaunch({ url: '/pages/home/home' });
+          })
+        }
+      }
+    })
   }
 })
